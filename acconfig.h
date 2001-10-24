8892d8e30fcc0b2f362d2c7729f1106c6607c82e
Robert J. Woźny <speedy@ziew.org>
2001-10-24 07:52:46 +0000

    autoconf w akcji... test it... stare configure w contrib ;>

---
+++ acconfig.h
@@ -0,0 +1,10 @@
+/* macros/curses checks */
+#undef HAS_CURSES
+#undef USE_SUNOS_CURSES
+#undef USE_BSD_CURSES
+#undef USE_SYSV_CURSES
+#undef USE_NCURSES
+#undef NO_COLOR_CURSES
+#undef SCO_FLAVOR
+
+#undef HAS_READLINE
