d1339ee34febcbb3c01add98fd672dc36d1801da
Wojtek Kaniewski <wojtekka@toxygen.net>
2001-11-02 23:06:57 +0000

    niech moc będzie z Tobą, tomee.

---
+++ examples/conn-async.c
@@ -0,0 +1,69 @@
+/* $Id$ */
+
+/*
+ * g³upiutki przyk³ad, jak siê po³±czyæ, korzystaj±c z asynchronicznych
+ * socketów. nic poza po³±czeniem nie robi. nie przejmuje siê b³êdami.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include "libgg.h"
+
+int main()
+{
+	struct gg_session *sess;
+	struct timeval tv;
+	struct gg_event *e;
+	fd_set rd, wd;
+	int ret;
+
+	gg_debug_level = ~0;
+	
+	sess = gg_login(535333, "dupa123", 1);
+
+	for (;;) {
+		FD_ZERO(&rd);
+		FD_ZERO(&wd);
+
+		if ((sess->check & GG_CHECK_READ))
+			FD_SET(sess->fd, &rd);
+		if ((sess->check & GG_CHECK_WRITE))
+			FD_SET(sess->fd, &wd);
+
+		tv.tv_sec = 10;
+		tv.tv_usec = 0;
+		
+		ret = select(sess->fd + 1, &rd, &wd, NULL, &tv);
+	
+		if (!ret) {
+			printf("timeout! wypad!\n");
+			return 1;
+		} else {
+			if (sess && (FD_ISSET(sess->fd, &rd) || FD_ISSET(sess->fd, &wd))) {
+				if (!(e = gg_watch_fd(sess))) {
+					printf("zerfauo pouontshenie!\n");
+					return 1;
+				}
+				if (e->type == GG_EVENT_CONN_SUCCESS) {
+					printf("po³±czy³em siê\n");
+					gg_logoff(sess);
+					gg_free_session(sess);
+					return 1;
+				}
+				if (e->type == GG_EVENT_CONN_FAILED) {
+					printf("errrror\n");
+					gg_logoff(sess);
+					gg_free_session(sess);
+					return 1;
+				}
+			}
+		}
+	}
+	
+	return -1;
+}
+
