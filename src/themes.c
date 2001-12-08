0b51875c979a6ca93330b24621d6f0b33cd9d380
Wojtek Kaniewski <wojtekka@toxygen.net>
2001-12-08 23:23:01 +0000

    DGCC.

--- src/themes.c
+++ src/themes.c
@@ -514,7 +514,7 @@ void init_theme()
 	add_format("chat_footer", "%c`----- ---- --- -- -%n\n", 1);
 	add_format("chat_line", "%c|%n %1\n", 1);
 	add_format("chat_line_width", "75", 1);
-	add_format("sysmsg_header", "%m.-- wiadomo¶æ systemowa %m--- -- -\n", 1);
+	add_format("sysmsg_header", "%m.-- %WWiadomo¶æ systemowa %m--- -- -\n", 1);
 	add_format("sysmsg_line", "%m|%n %1\n", 1);
 	add_format("sysmsg_line_width", "75", 1);
 	add_format("sysmsg_footer", "%m`----- ---- --- -- -%n\n", 1);	
