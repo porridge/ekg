7b3e831eb5ae6c39c6b991183f72d9dfec7214ed
Wojtek Kaniewski <wojtekka@toxygen.net>
2002-03-30 13:10:18 +0000

    fsck.

--- contrib/ekgnv.sh
+++ contrib/ekgnv.sh
@@ -6,6 +6,13 @@
 # -g || --get	    Sprawd¼ czy jest nowsza wersja i j± ¶ci±gnij.
 # -i || --install   Sprawd¼ czy jest nowsza wersj±, ¶ci±gnij j± i zainstaluj.
 #
+# Zmienne dla ~/.ekgnv :
+# WGET= ¶cie¿ka do wget'a
+# EKGTMP= gdzie ma wrzucaæ pliki tymaczasowe
+# EKGWWW= adres strony ekg.
+# EKGCONF= co ma podawaæ do configure gdy automatycznie budujesz ekg
+# LASTEKG= ostatnia zainstalowana werja ekg
+
 
 function options {
   echo "EKG new version by drJojo <jojo@slackware.pl>
@@ -21,11 +28,11 @@ exit 1
 if [ ! -f ~/.ekgnv ]; then \
  echo -n "Brak pliku z konfiguracj±. Tworze .ekgnv "
   touch ~/.ekgnv
-  echo "export WGET=`which wget`
-export TMP=/tmp
-export EKGWWW=http://dev.null.pl/ekg/
-export EKGCONF=\"--prefix=/usr --with-shared --with-ioctl\"
-export LASTEKG=00000000" >> ~/.ekgnv
+  echo "WGET=`which wget`
+EKGTMP=/tmp
+EKGWWW=http://dev.null.pl/ekg/
+EKGCONF=\"--prefix=/usr --with-shared --with-ioctl\"
+LASTEKG=ekg-00000000" >> ~/.ekgnv
  echo " Gotowe!"
  echo
 fi
@@ -45,10 +52,13 @@ function check_wget {
 function get_list {
  check_wget
  echo -n "¦ci±gam listê wersji EKG. Poczekaj chwilê. "
-  wget -q -P $TMP $EKGWWW/download.php
- LASTES="`grep ekg-20 $TMP/download.php | cut -d\  -f6 | \
+  wget -q -P $EKGTMP $EKGWWW/download.php
+ # to mozna zamienic na odczytywanie pliku ktory bylby automatycznie po
+ # twojej stronie generowany, a w ktorym bylby tylko numer najnowszej 
+ # werjsji
+ LASTES="`grep ekg-20 $EKGTMP/download.php | cut -d\  -f6 | \
   	   cut -d\\" -f2 | tail -1 | sed -e s/.tar.gz//`"
-  rm -f $TMP/download.php*
+  rm -f $EKGTMP/download.php*
  echo "Gotowe!"
 }
 
@@ -56,6 +66,8 @@ function get_list {
 function check_new {
  get_list
  NEW=0
+ # ten warunek jest wystraczalny, bo nie ma mozliwosci zeby pojawilas sie
+ # jakas starsza wersja
   if [ "$LASTES" != "$LASTEKG" ]; then \
     echo -n "Jest nowsza wersja $LASTES. "
     NEW=1
@@ -70,13 +82,13 @@ function get_new {
  check_new
   if [ ! -z "$NEW" ]; then \
    echo -n "¦ci±gam j±, poczekaj chwilê. "
-   wget -q -P $TMP $EKGWWW/$LASTES.tar.gz
+   wget -q -P $EKGTMP $EKGWWW/$LASTES.tar.gz
    cat ~/.ekgnv | sed -e s/$LASTEKG/$LASTES/ > ~/.ekgtmp
  # lub jak ktos nie ma seda to grep -v "LASTEKG" ~/.ekgnv > ~/.ekgtmp
  # echo "export LASTEKG=$LASTES" >> ~/.ekgtmp
    mv -f ~/.ekgtmp ~/.ekgnv
    echo "Gotowe.
-Plik znajduje siê w $TMP/$LASTES.tar.gz"
+Plik znajduje siê w $EKGTMP/$LASTES.tar.gz"
   else 
    echo 
   fi
@@ -87,7 +99,7 @@ function build_new {
  get_new
   if [ ! -z "$NEW" ]; then \
    echo -n "Buduje nowe EKG. Poczekaj chwilê. "
-   ( cd $TMP ;
+   ( cd $EKGTMP ;
    tar -zxf $LASTES.tar.gz ; cd $LASTES ;
      ./configure $EKGCONF > /dev/null ; # tylko b³êdy bêd± na konsoli.
       make ; make install > /dev/null ;
