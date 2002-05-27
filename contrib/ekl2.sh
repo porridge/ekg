a79e69238b6039b11518f755283fb78564533898
Wojtek Kaniewski <wojtekka@toxygen.net>
2002-05-27 22:50:00 +0000

    *** empty log message ***

--- contrib/ekl2.sh
+++ contrib/ekl2.sh
@@ -6,6 +6,7 @@
 # v1.1 (8.03.2002) poprawa drukowania nietypowych znakow, np. {}%*#$
 #	po wyslaniu ascii-rozyczki przez gg, w logach wyraznie wymagala ona naprawy
 # v1.2 (9.03.2002) poprawa wydajnosci. skrypt jest juz w miare szybki
+# v1.3 (28.05.2002) obsluga trybow logowania 1 i 2, paprawka dotyczaca ip
 
 
 ######
@@ -34,10 +35,10 @@ while [ $# -gt 0 ]; do
 		        echo -e "\t-h\t\tpomoc, któr± teraz widzisz"
 			echo -e "\t-L\t\tlista dostêpnych logów z katalogu $ggdir/history"
 			echo -e "\t-l\t\tlista sesji w wybranym logu"
-		        echo -e "\t-m name\t\dmoy¶lnie \$USER"
+		        echo -e "\t-m name\t\tdomoy¶lnie \$USER"
 		        echo -e "\t-n\t\tbez kolorów"
-			echo -e "\t-s date\t\tstart date; time from were to start printing dialog, see date(1) --date"
-		        echo -e "\t-t\t\tkeep timestamps interval"
+			echo -e "\t-s date\t\tstart date; czas, odk±d zacz±æ drukownie historii"
+		        echo -e "\t-t\t\tzachowaj odstêpy czasowe miêdzy wypowiedziami (symulacja rozmowy)"
 		        echo -e "\t-v\t\tversion"
 			exit
 		;;
@@ -45,9 +46,15 @@ while [ $# -gt 0 ]; do
 		-d) ggftimeformat="$2"; shift;;
 		-s) ggsdate="$2"; shift;;
 		-L) 
-			for i in `ls "$ggdir/history/"`; do
-				echo "$i "`sed "/$i/!d;s/;.*//;q" "${ggdir}/userlist"`
-			done
+			if [ -f "$ggdir/history" ]; then
+				for i in `sed "s/^[a-z]*,\([0-9]*\),.*/\1/;/[^0-9]/d;/^$/d" "$ggdir/history" | sort | uniq`; do
+					echo "$i "`sed "/$i/!d;s/;.*//;q" "${ggdir}/userlist"`
+				done
+			else
+				for i in `ls "$ggdir/history/"`; do
+					echo "$i "`sed "/$i/!d;s/;.*//;q" "${ggdir}/userlist"`
+				done
+			fi
 			exit
 		;;
 		-l) gglist="yes"; ggdlimit=10800 ;;
@@ -60,17 +67,21 @@ while [ $# -gt 0 ]; do
 		;;
 		*)
 			name=`sed "/^$1;/!d;s/.*;\([0-9]*\)/\1/;s/[^0-9]//;q" "$HOME/.gg/userlist"`
+			uin="$1"
 			dafcknfile="$HOME/.gg/history/$name"
-			[ -f "$thedir" ] && echo ok
-			if [ -f "$1" ]; then
-				gglog="$1";
-				elif [ -f "$ggdir/history/$1" ]; then
-					gglog="$ggdir/history/$1"; 
-					elif [ -f "$dafcknfile" ]; then
-						gglog="$dafcknfile";
-						else
-				        		echo "$0: log_file doesnt exists!"
-				        		exit 1
+			if [ -f "$HOME/.gg/history" ]; then	# set log 1
+				gglog="$HOME/.gg/history";
+			else
+				if [ -f "$1" ]; then			# set log 2
+					gglog="$1";
+					elif [ -f "$ggdir/history/$1" ]; then
+						gglog="$ggdir/history/$1"; 
+						elif [ -f "$dafcknfile" ]; then
+							gglog="$dafcknfile";
+							else
+				        			echo "$0: log_file doesnt exists!"
+				        			exit 1
+				fi
 			fi
 		;;
 	esac
@@ -78,16 +89,22 @@ while [ $# -gt 0 ]; do
 done
 
 IFS=$'\n'
-for i in `cat "$gglog"`; do
+for i in `cat "$gglog" | grep ",$uin,"`; do
 	gglasttime=$ggtime;
-	ggtime=`echo "$i" | cut -f 4 -d "," `
+	gg_4=`echo "$i" | cut -f 4 -d "," `
+	if [ -z "`echo "$gg_4" | sed "/[0-9]*\..*/d"`" ]; then
+		ggtime=`echo "$i" | cut -f 5 -d "," `;
+		ggip="(ip: $gg_4)";
+	else
+		ggtime=$gg_4;
+	fi
 	[ -z $gglasttime ] && gglasttime=0
 
 
 	if [ ! -z $gglist ] && [ ! -z $gglasttime ]; then
 		[ `expr $ggtime - $gglasttime` -lt $ggdlimit ] && continue
 		ggftime=`date --date "$(expr $ggtime - $ggtimenow)sec" +"$ggftimeformat"`
-		echo "GG session from: $ggftime"
+		echo "Sesja GG z: $ggftime $ggip"
 	fi
 
 	if [ ! -z "$ggsdate" ]; then
@@ -111,7 +128,7 @@ for i in `cat "$gglog"`; do
 		sleep "$ggwaittime";
 	fi
 
-	ggftime=`date --date "$(expr "$ggtime" - "$ggtimenow")sec" +"$ggftimeformat"` # UWAGA! linijka 90 taka sama!
+	ggftime=`date --date "$(expr "$ggtime" - "$ggtimenow")sec" +"$ggftimeformat"` # UWAGA! linijka 99 taka sama!
 	ggmsg=`echo "$i" | sed 's/.*,[0123456789]*,//'`
 	echo -e "${ggway}${gguid} [${ggftime}]: ${color:+\x1B[0;38m}\c"
 	printf "%s\n" "${ggmsg}"
