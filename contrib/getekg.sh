0b84680edd98f410cc82451ad640ecd6fa99f444
Wojtek Kaniewski <wojtekka@toxygen.net>
2002-05-29 15:36:17 +0000

    poprawki contribów.

--- contrib/getekg.sh
+++ contrib/getekg.sh
@@ -10,12 +10,21 @@ fi
 echo "Wchodzê do katalogu tymczasowego."
 cd /tmp
 echo -n "¦ci±gam najnowsz± wersjê."
-wget -o wget.log http://dev.null.pl/ekg/ekg-current.tar.gz
-rm wget.log
-echo -n " Rozpakowujê..."
-gunzip ekg-current.tar.gz
-tar xvf ekg-current.tar > /dev/null
-rm ekg-current.tar
+ if [ ! -x "`which wget`" ]; then \
+    if [ ! -x "`which curl`" ]; then \
+        if [ ! -x "`which lynx`" ]; then \
+            echo "Brak mo¿liwo¶ci ¶ci±gniêcia pliku (chyba, ¿e pod Xami)";
+            exit 1;
+        else 
+	    lynx -dump http://dev.null.pl/ekg/ekg-current.tar.gz | tar zxvf -
+	fi # lynx
+    else
+	curl -s http://dev.null.pl/ekg/ekg-current.tar.gz | tar xzf -
+    fi #curl
+ else
+    wget -q -O - http://dev.null.pl/ekg/ekg-current.tar.gz | tar xzf -
+ fi #wget
+echo -n " Rozpakowane.";
 cd `ls -1 | grep ekg-`
 echo -n " Konfigurujê..."
 ./configure $* > /dev/null
