273efe10673e4436e36c67911009991c8331ceb6
Wojtek Kaniewski <wojtekka@toxygen.net>
2002-11-12 16:39:28 +0000

    - drobne poprawki dokumentacji (szalik/w)

--- docs/ekg.man.pl
+++ docs/ekg.man.pl
@@ -1,9 +1,12 @@
-.TH EKG n "31 pa¼dziernika 2001" 
+.TH EKG n "11 listopada 2002" 
 .SH NAZWA
 ekg \- Eksperymentalny Klient Gadu-Gadu
 .SH SK£ADNIA
 .B ekg [
 .BI opcje
+.B ]
+.B [
+.BI komendy
 .B ]
 
 .SH OPIS
@@ -12,35 +15,44 @@ Jest to eksperymentalny klient Gadu-Gadu.
 
 .SH OPCJE
 .TP
-.BI \-d\ [\-\-debug]
-W³±cza tryb "debug" - jest to specjalny tryb, pokazuj±cy, co robi 
-program w danej chwili.
+.BI \-h\ [\-\-help]
+Wy¶wietla listê mo¿liwych opcji.
+.TP
+.BI \-u\ username\ [\-\-user\ username]
+Pozwala na uruchomienie ekg z innym ,,u¿ytkownikiem''
+Standardowo konfiguracja ekg znajduje sie w katalogu ~/.gg (chyba, ¿e
+zdefiniowana jest zmienna ¶rodowiskowa $CONFGI_DIR - wtedy w ~/$CONFIG_DIR/gg/).
+Ta opcja pozwala na stworzenie struktury podkatalogów z oddzielnymi
+konfiguracjami.
+.TP
+.BI \-t\ theme\ [\-\-theme\ theme]
+£aduje opis wygl±du z podanego pliku.
+.TP
+.BI \-c\ plik\ [\-\-control-pipe\ plik]
+Tworzy nazwany potok (ang. named pipe) umo¿liwiaj±cy sterowanie klientem.
 .TP
 .BI \-n\ [\-\-no-auto]
 Nie ³±cz siê automagicznie. Po uruchomieniu nie ³±czy siê automatycznie 
 z serwerami gg. 
 .TP
-.BI \-u\ username\ [\-\-user\ username]
-Zezwala na uruchomienie ekg z innym \"u¿ytkownikiem\". 
-Standardowo konfiguracja ekg znajduje sie w katalogu ~/.gg (chyba, ¿e
-zdefiniowana jest zmienna ¶rodowiskowa $CONFGI_DIR - wtedy w ~/$CONFIG_DIR/gg/)
-Ta opcja pozwala na stworzenie struktury podkatalogów z oddzielnymi
-konfiguracjami.
+.BI \-a\ [\-\-away[=opis]]
+Po po³±czeniu zmienia stan na ,,zajêty''.
 .TP
-.BI \-t\ theme\ [\-\-theme\ theme]
-³aduje opis wygl±du z podanego pliku
-.TP
-.BI \-a\ [\-\-away]
-po po³±czeniu zmienia stan na ,,zajêty''
-.TP
-.BI \-b\ [\-\-back]
-po po³±czeniu zmienia stan na ,,dostêpny''
+.BI \-b\ [\-\-back[=opis]]
+Po po³±czeniu zmienia stan na ,,dostêpny''.
 .TP 
-.BI \-i\ [\-\-invisible]
-po po³±czeniu zmienia stan na ,,niewidoczny''
+.BI \-i\ [\-\-invisible[=opis]]
+Po po³±czeniu zmienia stan na ,,niewidoczny''.
 .TP
 .BI \-p\ [\-\-private]
-po po³±czeniu zmienia stan na ,,tylko dla przyjació³''
+Po po³±czeniu w³±cza tryb ,,tylko dla przyjació³''.
+.TP
+.BI \-d\ [\-\-debug]
+W³±cza tryb ,,debug'' - jest to specjalny tryb, pokazuj±cy, co robi 
+program w danej chwili.
+.TP
+.BI \-v\ [\-\-version]
+Wy¶wietla wersjê klienta i biblioteki libgadu.
 .TP
 .BI \-f\ nazwa\ [\-\-frontend\ nazwa]
-wybiera podany interfejs u¿ytkownika
+Wybiera podany interfejs u¿ytkownika.
