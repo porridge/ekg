7ed94f99532101f1918185803e645488b17ff643
Wojtek Kaniewski <wojtekka@toxygen.net>
2002-11-25 17:10:48 +0000

    - mea culpa (w)

--- compat/scandir.h
+++ compat/scandir.h
@@ -8,7 +8,7 @@
 
 #include <dirent.h>
 
-int foo_alphasort(const struct dirent **a, const struct dirent **b);
-int foo_scandir(const char *dir, struct dirent ***namelist, int (*select)(const struct dirent *), int (*compar)(const struct dirent **, const struct dirent **));
+int alphasort(const struct dirent **a, const struct dirent **b);
+int scandir(const char *dir, struct dirent ***namelist, int (*select)(const struct dirent *), int (*compar)(const struct dirent **, const struct dirent **));
 
 #endif
