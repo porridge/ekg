93135fe7b011d596099f3c4fccf5e406f5fa75e3
Wojtek Kaniewski <wojtekka@toxygen.net>
2002-12-07 21:04:36 +0000

    - be free, yeah (; (w)

--- src/ui-ncurses.c
+++ src/ui-ncurses.c
@@ -2130,6 +2130,8 @@ static void binding_add(const char *key, const char *action, int quiet)
 
 			if (!strcmp(b.key, foo->key)) {
 				print("bind_seq_exist", b.key); 
+				xfree(b.key);
+				xfree(b.action);
 				return;
 			}
 		}
