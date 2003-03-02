58dcb99748124ab2931844b472e392dd92f6d3bf
Piotr Domagalski <szalik@szalik.net>
2003-03-02 20:27:13 +0000

    - nowe man'y dla ekl2 (porridge/sz)

--- docs/ekl2.man.pl
+++ docs/ekl2.man.pl
@@ -1,40 +1,47 @@
-.TH EKL2 1 "23 Maja 2002" 
+.TH EKL2 1 "2 Marca 2003" 
 .SH NAZWA
 ekl2 \- Podgl±d historii Eksperymentalnego Klienta Gadu-Gadu.
 .SH SK£ADNIA
-.B ekl2 [-d format] [-h] [-l] [-m name] [-n] [-s date] [-t] [-v] log_file [
-.BI opcje
+.B ekl2 [
+.BI OPCJE
+.B ] [
+.BI ¬RÓD£O
 .B ]
-
 .SH OPIS
 .B ekl2
 Jest to aplikacja umo¿liwiaj±ca podgl±d historii rozmów, tworzonej przez
 Eksperymentalnego Klienta Gadu-Gadu.
 .SH OPCJE
 .TP
-.BI \-d\ format
-Ustala format wyswietlanej daty (zobacz date(1))
+.BI \-d\ +format\ \-\-date\ +format\ 
+Ustala format wy¶wietlanej daty (zobacz date(1)).
 .TP
-.BI \-h\ 
+.BI \-\-location\ 
+Ustala miejsce przechowywania plików dziennika (domy¶lnie ~/.gg/history).
+.TP
+.BI \-h\ \-\-help\ 
 Wy¶wietla pomoc.
 .TP
-.BI \-L\ 
-Pokazuje listê dostêpnych logow. (z katalogu ~\/.gg\/history)
+.BI \-L\ \-\-List\ 
+Pokazuje listê dostêpnych logow. (z katalogu ~/.gg/history).
 .TP
-.BI \-l\ 
+.BI \-l\ \-\-sessions\ [+num]\ [+ods]
 Pokazuje listê sesji w podanym logu.
+Nieobowi±zkowy parametr
+.BI num\ 
+to numer sesji, która ma zostaæ wy¶wietlona, a 
+.BI ods\ 
+to odstêp miêdzy rozmowami, w minutach (domy¶lnie: 60)
 .TP
-.BI \-m\ nazwa
-Swój w³asny nick (domy¶lnie $USER)
-.TP
-.BI \-s\ data
-Data od której ma zacz±æ wy¶wietlaæ historiê. (zobacz date(1))
-.TP
-.BI \-n\ 
+.BI \-n\ \-\-nocolor\ 
 Bez kolorków.
-.TP 
-.BI \-t\ 
-Wy¶wietla historiê zachowuj±c dostêpy czasowe w jakich nap³ywa³y wiadomo¶ci.
+.BI \-sh\ \-\-short\ 
+simplified form.
 .TP
-.BI \-v\ 
-Pokazuje wersje skrytpu.
+.BI \-v\ \-\-version\ 
+Prints script version.
+.TP
+.BI ¬RÓD£O\ 
+numer gg, nick lub nazwa pliku z logami.
+.SH "PATRZ TE¯"
+.BR ekg (1).
