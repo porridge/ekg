4ab2d1bc9488bf0fe722e03b61725a140f794c57
Arkadiusz Miśkiewicz <arekm@pld-linux.org>
2004-04-23 22:43:59 +0000

    - Fix my email.

--- lib/libgadu.h
+++ lib/libgadu.h
@@ -3,7 +3,7 @@
 /*
  *  (C) Copyright 2001-2003 Wojtek Kaniewski <wojtekka@irc.pl>
  *                          Robert J. Wo¼ny <speedy@ziew.org>
- *                          Arkadiusz Mi¶kiewicz <misiek@pld.org.pl>
+ *                          Arkadiusz Mi¶kiewicz <arekm@pld-linux.org>
  *                          Tomasz Chiliñski <chilek@chilan.com>
  *                          Piotr Wysocki <wysek@linux.bydg.org>
  *                          Dawid Jarosz <dawjar@poczta.onet.pl>
