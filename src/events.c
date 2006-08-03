25bf5b9a7caafa24c495c63b438a4526992f0980
Adam Wysocki <gophi@supermedia.pl>
2006-08-03 04:27:47 +0000

    - poprawienie signed/unsigned (a przez to psucia polskich znaków)   związanego z dcc i obrazkami (eMBe/g) - wyodrębnienie fix_filename() (gophi)

--- src/events.c
+++ src/events.c
@@ -2151,6 +2151,27 @@ void handle_disconnect(struct gg_event *e)
 }
 
 /*
+ * fix_filename()
+ *
+ * poprawia nazwê pliku do zapisania na dysku, usuwaj±c potencjalnie 
+ * niebezpieczne znaki - elementy ¶cie¿ki oraz znaki o kodach < 32. 
+ * u¿ywane przez obs³ugê dcc oraz obrazków.
+ *
+ * - name - nazwa
+ */
+static void fix_filename (unsigned char *name)
+{
+	unsigned char *p;
+
+	for (p = name; *p; p++)
+		if (*p < 32 || *p == '\\' || *p == '/')
+			*p = '_';
+
+	if (name[0] == '.')
+		name[0] = '_';
+}
+
+/*
  * find_transfer()
  *
  * znajduje strukturê ,,transfer'' dotycz±c± danego po³±czenia.
@@ -2370,12 +2391,7 @@ void handle_dcc(struct gg_dcc *d)
 				t = list_add(&transfers, &tt, sizeof(tt));
 			}
 
-			for (p = d->file_info.filename; *p; p++)
-				if (*p < 32 || *p == '\\' || *p == '/')
-					*p = '_';
-
-			if (d->file_info.filename[0] == '.')
-				d->file_info.filename[0] = '_';
+			fix_filename(d->file_info.filename);
 
 			t->type = GG_SESSION_DCC_GET;
 			t->filename = xstrdup(d->file_info.filename);
@@ -2730,19 +2746,12 @@ void handle_image_reply(struct gg_event *e)
 		/* to nie jest spy */
 
 		if (config_receive_images) {
-			char *fname, *path;
+			unsigned char *fname, *path;
 			int fd;
-			size_t i, sz;
 			ssize_t rs;
 
 			fname = xstrdup(e->event.image_reply.filename);
-			sz = strlen(fname);
-			for (i = 0; i < sz; i++)
-				if (fname[i] < 32 || fname[i] == '\\' || fname[i] == '/')
-					fname[i] = '_';
-
-			if (fname[0] == '.')
-				fname[0] = '_';
+			fix_filename(fname);
 
 			if (config_dcc_dir)
 				path = saprintf("%s/%s", config_dcc_dir, fname);
@@ -2750,6 +2759,7 @@ void handle_image_reply(struct gg_event *e)
 				path = xstrdup(fname);
 
 			xfree(fname);
+			cp_to_iso(path);
 
 			gg_debug(GG_DEBUG_MISC, "// ekg: trying to save image: %s\n", path);
 
